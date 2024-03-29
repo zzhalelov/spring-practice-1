bf6f0d2559cd525cd38dd56994df07042fe03aca
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-03-29 22:57:20 +0500

    fix: add if-else

--- src/main/java/kz/runtime/springpractice1/controller/TemperatureController.java
+++ src/main/java/kz/runtime/springpractice1/controller/TemperatureController.java
@@ -9,14 +9,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class TemperatureController {
     @GetMapping("/converter")
-    public String converterForm() {
+    public String get() {
         return "converter";
     }
 
     @PostMapping("/converter")
-    public String converterTemperature(@RequestParam double inputValue, Model model) {
-
-
-        return "result";
+    public String convert(@RequestParam("value") double value, @RequestParam("select") String select, Model model) {
+        double result;
+        if (select.equals("F")) {
+            result = ((value * 9) / 5) + 32;
+        } else {
+            result = (value * 9 / 5) + 32;
+        }
+        model.addAttribute("result", result);
+        return "converter";
     }
 }
