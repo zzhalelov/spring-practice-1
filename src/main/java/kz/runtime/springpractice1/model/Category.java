e83de1b1a08f3cdd47994b432e4c0311b1182e72
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-03-30 22:46:30 +0500

    feat: create class Category.java

---
+++ src/main/java/kz/runtime/springpractice1/model/Category.java
@@ -0,0 +1,10 @@
+package kz.runtime.springpractice1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Category {
+    private String name;
+}
